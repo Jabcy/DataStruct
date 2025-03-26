064ffc8b864842072d5a9fb530252ea19697a649
Jabcy <angjie26@163.com>
2025-03-26 20:32:16 +0800

    实现了以递归实现阶乘和以递归实现斐波那契数列，增加了前缀表达式计算和后缀表达计算式的逻辑注释，增加了中缀转后缀表达式的逻辑注释

--- Stack/LinkStackWithHeader.c
+++ Stack/LinkStackWithHeader.c
@@ -72,32 +72,83 @@ bool bracketCheck(char arr[],int length){
     return isEmpty(S);
 }
 
+// TODO...
+// 中缀转后缀表达式
+// 1.从左到右扫描表达式
+// 2.遇到操作符就直接加入后缀表达式
+// 3.遇到左括号直接入栈
+// 4.遇到有括号则弹出栈顶元素加入后缀表达式直到左括号,括号不加入后缀表达式
+// 5.遇到运算符,就依次弹出栈内"优先级"高于或等于本运算符的运算符,并加入后缀表达式
+//       将本运算符压入栈,碰到左括号停止,进行下一次扫描
+// 6.将中缀表达式扫描完后,依次弹出栈顶元素,并加入后缀表达式
+
+// TODO...
+// 后缀表达式(逆波兰式)的计算
+// 1.从左到右扫描表达式
+// 2.如果是操作数就压入栈中，并回到 1
+// 3.如果是运算符，就弹出两个栈顶元素，并执行运算，先弹出的在运算符右边，后弹出的在运算符左边，结果压入栈中
+// TODO...
+bool calculateSuffixExpression(){
+
+}
+
+// TODO...
+// 前缀表达式(波兰式)的计算
+// 1.从右到左扫描表达式
+// 2.如果是操作数就压入栈中，并回到 1
+// 3.如果是运算符，就弹出两个栈顶元素，并执行运算，先弹出的在运算符左边，后弹出的在运算符右边，结果压入栈中
+// TODO...
+bool calculatePrefixExpression(){
+
+}
+
+
+// 递归实现阶乘
+int factorial(int n){
+    if(n==0||n==1){return 1;}
+    else return n*factorial(n-1);
+}
+
+
+// 递归实现斐波那契数列
+int fib(int n){
+    if(n==0||n==1){return n;}
+    else return fib(n-2)+fib(n-1);
+    
+}
 
 int main()
 {
 
- // 测试用例
- char arr1[6] = {'(', '(', ')', ')', '(', ')'}; // 匹配成功
- char arr2[6] = {'(', '(', '(', ')', ')', ')'}; // 匹配成功
- char arr3[6] = {'(', ')', ')', '(', ')', ')'}; // 多余右括号
- char arr4[6] = {'(', '(', ')', ')', ')', ')'}; // 右括号过多
- char arr5[6] = {'(', '[', ']', ')', '{', ')'}; // 类型不匹配
- char arr6[6] = {'(', '[', '{', '}', ']', ')'}; // 嵌套匹配成功
- char arr7[6] = {'(', '(', '(', '(', '(', '('}; // 只有左括号
- char arr8[6] = {')', ')', ')', ')', ')', ')'}; // 只有右括号
- char arr9[0] = {};                             // 空数组
- char arr10[1] = {'('};                         // 单独左括号
+    // // 测试用例
+    // char arr1[6] = {'(', '(', ')', ')', '(', ')'}; // 匹配成功
+    // char arr2[6] = {'(', '(', '(', ')', ')', ')'}; // 匹配成功
+    // char arr3[6] = {'(', ')', ')', '(', ')', ')'}; // 多余右括号
+    // char arr4[6] = {'(', '(', ')', ')', ')', ')'}; // 右括号过多
+    // char arr5[6] = {'(', '[', ']', ')', '{', ')'}; // 类型不匹配
+    // char arr6[6] = {'(', '[', '{', '}', ']', ')'}; // 嵌套匹配成功
+    // char arr7[6] = {'(', '(', '(', '(', '(', '('}; // 只有左括号
+    // char arr8[6] = {')', ')', ')', ')', ')', ')'}; // 只有右括号
+    // char arr9[0] = {};                             // 空数组
+    // char arr10[1] = {'('};                         // 单独左括号
 
- // 测试输出
- printf("Test Case 1: %d\n", bracketCheck(arr1, 6)); // 1
- printf("Test Case 2: %d\n", bracketCheck(arr2, 6)); // 1
- printf("Test Case 3: %d\n", bracketCheck(arr3, 6)); // 0
- printf("Test Case 4: %d\n", bracketCheck(arr4, 6)); // 0
- printf("Test Case 5: %d\n", bracketCheck(arr5, 6)); // 0
- printf("Test Case 6: %d\n", bracketCheck(arr6, 6)); // 1
- printf("Test Case 7: %d\n", bracketCheck(arr7, 6)); // 0
- printf("Test Case 8: %d\n", bracketCheck(arr8, 6)); // 0
- printf("Test Case 9: %d\n", bracketCheck(arr9, 0)); // 1
- printf("Test Case 10: %d\n", bracketCheck(arr10, 1)); // 0
+    // // 测试输出
+    // printf("Test Case 1: %d\n", bracketCheck(arr1, 6)); // 1
+    // printf("Test Case 2: %d\n", bracketCheck(arr2, 6)); // 1
+    // printf("Test Case 3: %d\n", bracketCheck(arr3, 6)); // 0
+    // printf("Test Case 4: %d\n", bracketCheck(arr4, 6)); // 0
+    // printf("Test Case 5: %d\n", bracketCheck(arr5, 6)); // 0
+    // printf("Test Case 6: %d\n", bracketCheck(arr6, 6)); // 1
+    // printf("Test Case 7: %d\n", bracketCheck(arr7, 6)); // 0
+    // printf("Test Case 8: %d\n", bracketCheck(arr8, 6)); // 0
+    // printf("Test Case 9: %d\n", bracketCheck(arr9, 0)); // 1
+    // printf("Test Case 10: %d\n", bracketCheck(arr10, 1)); // 0
+
+
+    //  阶乘测试
+    int x = factorial(3);
+    printf("%d\n",x);
+    x = fib(6);
+    printf("%d\n",x);
     return 0;
 }
